c743c57cda5f2226ba1086c55052f03ba2a71c7c
yohannes2025 <johannes.m.tekle@gmail.com>
2025-02-18 14:29:08 +0300

    feat: Add static file settings

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +126,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # Corrected: Use Pathlib for static files
+
+# Directory where collectstatic will store all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Optional: Additional locations for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
